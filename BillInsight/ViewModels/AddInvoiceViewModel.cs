eb8a9b3a9dc65beb4ab46afc5a5b7688de3be224
VinhTin-AQUA <tinhovinh@gmail.com>
2025-10-10 23:15:03 +0700

    update

--- BillInsight/ViewModels/AddInvoiceViewModel.cs
+++ BillInsight/ViewModels/AddInvoiceViewModel.cs
@@ -1,12 +1,38 @@
 using System;
+using System.Collections.ObjectModel;
+using System.Reactive;
+using BillInsight.Models.Products;
+using ReactiveUI;
 
 namespace BillInsight.ViewModels
 {
     public class AddInvoiceViewModel : ViewModelBase
     {
+        private string _name;
+        private decimal _price;
+
+        public string Name
+        {
+            get => _name;
+            set => this.RaiseAndSetIfChanged(ref _name, value);
+        }
+
+        public decimal Price
+        {
+            get => _price;
+            set => this.RaiseAndSetIfChanged(ref _price, value);
+        }
+        
+
+        public ObservableCollection<ProductModel> Products { get; set; } =
+        [
+            new ProductModel() { Name = "Sản phẩm A", Price = 10000, PaymentMethod = "A" },
+            new ProductModel() { Name = "Sản phẩm B", Price = 15000, PaymentMethod = "B" },
+            new ProductModel() { Name = "Sản phẩm C", Price = 20000, PaymentMethod = "C" }
+        ];
+        
         public AddInvoiceViewModel()
         {
-            Console.WriteLine("Hello World!");
         }
         
         public override void Dispose()
--- BillInsight/Views/AddInvoiceView.axaml.cs
+++ BillInsight/Views/AddInvoiceView.axaml.cs
@@ -1,6 +1,8 @@
+using System.IO;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
 
 namespace BillInsight.Views
 {
@@ -9,6 +11,31 @@ namespace BillInsight.Views
         public AddInvoiceView()
         {
             InitializeComponent();
+        }
+        
+        private async void OnChooseImageClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                AllowMultiple = false,
+                Filters =
+                {
+                    new FileDialogFilter() { Name = "Ảnh", Extensions = { "png", "jpg", "jpeg", "bmp" } }
+                }
+            };
+
+            string[]? result = await dialog.ShowAsync(this);
+
+            if (result != null && result.Length > 0)
+            {
+                string filePath = result[0];
+
+                // Mở file ảnh và gán vào Image control
+                using (var stream = File.OpenRead(filePath))
+                {
+                    PreviewImage.Source = new Bitmap(stream);
+                }
+            }
         }
     }
 }
